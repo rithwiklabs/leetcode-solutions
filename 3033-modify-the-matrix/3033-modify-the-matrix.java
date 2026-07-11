527cbab036581ecee260666ad0aee7945dbd4b66
M Rithwik Kumar <rithwik15052007@gmail.com>
2026-07-11 20:46:31 +0530

    Time: 1 ms (100%), Space: 47.4 MB (47.02%) - LeetHub

---
+++ 3033-modify-the-matrix/3033-modify-the-matrix.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[][] modifiedMatrix(int[][] matrix) {
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int[] colmax=new int[n];
+        Arrays.fill(colmax,Integer.MIN_VALUE);
+        
+        for (int i=0;i<m;i++) {
+            for (int j=0;j<n;j++) {
+                colmax[j]=Math.max(colmax[j],matrix[i][j]);
+            }
+        }
+
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==-1)
+                    matrix[i][j]=colmax[j];
+            }
+        }
+        return matrix;
+    }
+}
