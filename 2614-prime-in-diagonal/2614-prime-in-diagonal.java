a1a66d5745b1d89e02bbbf8934fc6dc0718ad1f2
M Rithwik Kumar <rithwik15052007@gmail.com>
2026-07-14 21:33:50 +0530

    Time: 4 ms (77.16%), Space: 82.4 MB (78.92%) - LeetHub

---
+++ 2614-prime-in-diagonal/2614-prime-in-diagonal.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isPrime(int n)
+    {
+        if(n<=1)
+            return false;
+        for(int i=2;i*i<=n;i++)
+        {
+            if(n%i==0)
+                return false;
+        }
+        return true;
+    }
+    public int diagonalPrime(int[][] nums) {
+        int n=nums.length;
+        int ans=0;
+        for(int i=0;i<n;i++)
+        {
+            if(isPrime(nums[i][i]))
+                ans=Math.max(ans,nums[i][i]);
+            
+            if(isPrime(nums[i][n-i-1]))
+                ans=Math.max(ans,nums[i][n-i-1]);
+        }
+        return ans;
+    }
+}
