1511de71fe2eff781bffe7d46616af741958cd72
M Rithwik Kumar <rithwik15052007@gmail.com>
2026-07-27 19:00:33 +0530

    Time: 1 ms (89.01%), Space: 43.2 MB (62.55%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int count=0;
+        for(int i=0;i<word.length();i++)
+        {
+            if(word.charAt(i) >='A' && word.charAt(i) <='Z')
+                count++;
+        }
+        if(count==word.length() || count==0 || count==1 && word.charAt(0) >='A' && word.charAt(0) <='Z')
+            return true;
+            
+        return false;
+    }
+}
