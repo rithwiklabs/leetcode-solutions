5a9baaaa2a5e24e23fed886a2effb1d6f05d1572
M Rithwik Kumar <rithwik15052007@gmail.com>
2026-07-28 19:22:47 +0530

    Time: 43 ms (14.06%), Space: 48.4 MB (10.04%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.java
@@ -3,25 +3,20 @@ class Solution {
         int n = s.length();
         String first;
         char mid = ' ';
-
         if (n % 2 == 0)
             first = s.substring(0, n / 2);
         else {
             first = s.substring(0, n / 2);
             mid = s.charAt(n / 2);
         }
-
         char[] fst = first.toCharArray();
         Arrays.sort(fst);
-
         StringBuilder ans = new StringBuilder();
-
         for (char ch : fst) {
             ans.append(ch);
         }
         StringBuilder rev = new StringBuilder(ans);
         rev.reverse();
-
         if(n%2==0)
             return ans.toString()+rev.toString();
         else
