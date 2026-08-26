59fa8911f719bc1598feb671d554306bd6236d2b
M Rithwik Kumar <rithwik15052007@gmail.com>
2026-08-26 06:45:10 +0530

    Time: 2 ms (26.47%), Space: 43.3 MB (37.42%) - LeetHub

--- 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.java
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.java
@@ -1,23 +1,39 @@
+// class Solution {
+//     public boolean checkAlmostEquivalent(String word1, String word2) {
+//         Map<Character,Integer> m1 = new HashMap<>();
+//         Map<Character,Integer> m2 = new HashMap<>();
+//         for(char ch : word1.toCharArray())
+//         {
+//             m1.put(ch,m1.getOrDefault(ch,0)+1);
+//         }
+//         for(char ch : word2.toCharArray())
+//         {
+//             m2.put(ch,m2.getOrDefault(ch,0)+1);
+//         }
+//         for(char ch = 'a'; ch<='z';ch++)
+//         {
+//             int c1 = m1.getOrDefault(ch,0);
+//             int c2 = m2.getOrDefault(ch,0);
+//             if(Math.abs(c1-c2)>3)
+//                 return false;
+//         }
+
+//         return true;
+//     }
+// }
 class Solution {
     public boolean checkAlmostEquivalent(String word1, String word2) {
-        Map<Character,Integer> m1 = new HashMap<>();
-        Map<Character,Integer> m2 = new HashMap<>();
-        for(char ch : word1.toCharArray())
+        Map<Character,Integer> m = new HashMap<>();
+        for(int i=0;i<word1.length();i++)
         {
-            m1.put(ch,m1.getOrDefault(ch,0)+1);
+            m.put(word1.charAt(i),m.getOrDefault(word1.charAt(i),0)+1);
+            m.put(word2.charAt(i),m.getOrDefault(word2.charAt(i),0)-1);
         }
-        for(char ch : word2.toCharArray())
+        for(int i : m.values())
         {
-            m2.put(ch,m2.getOrDefault(ch,0)+1);
-        }
-        for(char ch = 'a'; ch<='z';ch++)
-        {
-            int c1 = m1.getOrDefault(ch,0);
-            int c2 = m2.getOrDefault(ch,0);
-            if(Math.abs(c1-c2)>3)
+            if( i>3 || i<-3)
                 return false;
         }
-
         return true;
     }
 }
